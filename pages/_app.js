d52ad26d3817f76647e34c35cb245d63891f1583
builtbysilo <cody@builtbysilo.com>
2023-01-23 15:01:57 -0600

    SetUp

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,25 @@
-import '@styles/globals.css'
+import React from "react";
+import Head from "next/head";
+import {ChakraProvider, extendTheme} from "@chakra-ui/react";
+// import { AppWrapper } from '../context/state.js';
+import {AnimatePresence} from "framer-motion";
+// import Layout from "../components/Layout";
 
-function Application({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function App({Component, pageProps}) {
+  const theme = extendTheme({
+    // fonts: {
+    //   heading: `'Bai Jamjuree', sans-serif`,
+    //   body: `'Bai Jamjuree', sans-serif`,
+    // },
+  });
+  return (
+    <AnimatePresence>
+      <ChakraProvider resetCSS theme={theme}>
+          <Head>
+            <link rel="shortcut icon" href="/crown.svg" />
+          </Head>
+          <Component {...pageProps} />
+      </ChakraProvider>
+    </AnimatePresence>
+  );
 }
-
-export default Application
--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,45 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import {
+  Flex,
+  Grid,
+  GridItem,
+  Text,
+  Heading,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  useDisclosure,
+  Lorem,
+  Button,
+} from "@chakra-ui/react";
+
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="container">
-      <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
-
-      <Footer />
-    </div>
-  )
+    <Flex
+      h="100vh"
+      w="100vw"
+      bgColor="#fff"
+      justifyContent="center"
+      alignItems="center"
+      flexDirection="column"
+      gap="4"
+    >
+      <Image src="/pvcking-license.jpg" width="200" height="100"></Image>
+      <Heading size="xl">The PVC King</Heading>
+      <Heading size="md">You can tell by my license plate</Heading>
+      <iframe
+        src="https://giphy.com/embed/QCQZ1wjZFAXM6P5wjL"
+        width="480"
+        height="270"
+        frameBorder="0"
+        class="giphy-embed"
+        allowFullScreen
+      ></iframe>
+    </Flex>
+  );
 }
